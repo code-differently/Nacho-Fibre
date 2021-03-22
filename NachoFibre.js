f8e648f3e40f83cca6b1d6b32182557ddad1eac7
Hizzle 302 <azcrawley@yahoo.com>
2021-03-22 19:18:02 -0400

    kaleb problem

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,11 +2,29 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+        // *fibonacci(0) = 0*
+// * *fibonacci(1) = 1*
+// * *fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)*
+
+// Given *n*, return the *nth* number in the sequence.
+     let nth = []
+
+     for (let i = 0; i < array.length; i++){
+         if(n.length > n - 1 + n - 2);
+     }
+
+
+
+        return nth;
     }
 
     kaleb(n){
-        return -1;
+        let nth = []
+        for(let i = 0; i < array.length; i++){
+            if(n.length > 2 * n - 2 - n - 1);
+        }
+
+        return nth;
     }
 
 }
