631c1d7ad9754899ff76f5b359602d507e23c4d4
Denae716 <denaebrown73@yahoo.com>
2021-03-22 19:20:24 -0400

    03/22/2021

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,7 +2,17 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+        // [0, 1, 1, 2, 3, 5, 8] so the 6th number is 8.
+        //create an array that contains the first two entries
+        let sequence = [0, 1, 1, 2, 3, 5, 8]
+        let i = sequence[i]
+        
+        // create a fuction that will create/add to an array and calc
+        let n = sequence.push([i-1] + [i-2])
+       
+
+        
+        return n
     }
 
     kaleb(n){
