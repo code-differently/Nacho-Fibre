4c461286f6a56da0da12800cf8983b872b526d47
Robert Woodruff <rwoodruff@Roberts-Air.fios-router.home>
2021-03-22 19:19:58 -0400

    test case 1 submission

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,12 +1,22 @@
 
-class NachoFibre{
+class NachoFibre {
 
-    fibonacci(n){
-        return -1;
+    fibonacci(n) {
+        // declare variable to hold fibonacci output
+        let fibonacci;
+        
+        // populate 1st two index's  with known numbers and cycle throgh and populate remaining index's
+        for (let i = 0, j = 2; i < 2, j < n; i++, j++) {
+            fibonacci[i] = i;
+            fibonacci[j] = (j - 1) + (j - 2);
+        }
+        //output result
+        //console.log(fibonacci)
+        return fibonacci;
     }
 
-    kaleb(n){
-        return -1;
+    kaleb(n) {
+        return -1
     }
 
 }
