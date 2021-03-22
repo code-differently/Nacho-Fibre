4343f1c00820458dffa1f86dbc6f1a4d02c050d9
Nkenna Ugoanusi <nkanusi@gmail.com>
2021-03-22 19:00:49 -0400

    first commit

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,11 +2,24 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+
+       if(n <= 1){
+           return n;
+       } else {
+        return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+       }
+
+        // return b;
     }
 
     kaleb(n){
-        return -1;
+      if (n<= 1) {
+          return n;
+      }
+      else {
+        return 2 * this.kaleb(n - 2) - this.kaleb(n - 1 );
+    }
+        // return -1;
     }
 
 }
