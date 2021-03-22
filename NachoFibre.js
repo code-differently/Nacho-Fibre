b3e7a66b83879d948de4c6cfb766d822eda00c74
Hakim Mackey <hakimmackey@Hakims-Air.fios-router.home>
2021-03-22 19:17:23 -0400

    added 3 22 21

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,9 +1,26 @@
 
 class NachoFibre{
+// write a statement that adds two indexes
 
     fibonacci(n){
+        for (let i = 0; i <= 0; i++) {
+
+        }
+// then each following index is equal to the sum of the previous two indexes
+
+// then select the input numeral
+
+// then loop/cycle through the array of indexes till you reach the input numeral position
+
+// then output the given number
+        
         return -1;
     }
+
+
+// write a statement that multiplies the previous 
+
+// then 
 
     kaleb(n){
         return -1;
@@ -12,3 +29,6 @@ class NachoFibre{
 }
 
 module.exports = NachoFibre;
+
+
+I drew a total blank on how to handle this ...
