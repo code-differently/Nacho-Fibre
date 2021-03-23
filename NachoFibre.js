cdbb009025bfdef2679619ea687d12740e97da15
Kim Lai <kimlai@Kims-MacBook-Air.local>
2021-03-23 19:16:18 -0400

    fibo

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,7 +2,11 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+        if(n==1)
+            return 0;
+        if (n == 2)
+            return 1;
+        return this.fibonacci(n - 1) + fibonacci(n - 2);
     }
 
     kaleb(n){
