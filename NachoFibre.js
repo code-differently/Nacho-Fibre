9b9ff435ec5339916c2a1f8c356562ca716de559
Mike VanMatre <74632502+Da-ZooKa@users.noreply.github.com>
2021-03-23 19:21:14 -0400

    no solution

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,7 +2,21 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+     var a = 0;
+     var b = 1;
+     var c = a+b;
+     var i=0;
+     for (i = 0; i < n; i++ ) {
+      let c = a+b;   
+      let a = b;
+      let b = c;
+     } return n;
+    //   for (i = 0; i < num; i++) 
+    //   {
+    //       sum=num1+num2;
+    //       num1=num2;
+    //       num2=sum;
+    //   }
     }
 
     kaleb(n){
