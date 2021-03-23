f065dd86e94e145629c110556cb9f1c1463e1852
Bowen Stone <bowenstone@comcast.net>
2021-03-23 14:25:28 -0400

    Fibo works; Kaleb does not;

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,14 +1,46 @@
-
+console.log('and again...')
 class NachoFibre{
-
-    fibonacci(n){
-        return -1;
+    constructor(fibo, kaleb)    {   
+        this.fibo = fibo;
+        this.kaleb = kaleb;
     }
 
-    kaleb(n){
-        return -1;
+    // set prev to 0 & sum to 1
+    // add the last two numbers until you get to n
+    // return final sum
+
+    fibonacci(fibo){
+        let prev=0;
+        let curr=1;
+        let result=0;
+        for (let i=1; i<fibo; ++i)    {
+            result = curr + prev;
+            prev = curr;
+            curr = result;
+        } 
+        console.log(result);
+        return result;
+    }
+
+    // set prev to 2 & curr to -1
+    // double the prev term minus the curr term, n times
+    // return final result
+
+    kaleb(kaleb){
+        let prev=2;
+        let curr=-1;
+        let result=0;
+        for (let i=1; i<kaleb; ++i)    {
+            result = (2*prev) - curr;
+            prev = curr;
+            curr = result;
+        } 
+        console.log(result);
+        return result;
     }
 
 }
+
+//let assess = new NachoFibre(6,6); 
 
 module.exports = NachoFibre;
