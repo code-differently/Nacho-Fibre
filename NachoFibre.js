4eac61d316db1e5747e8ea88df1525e9069baed3
cv-net <khalil.mktg@gmail.com>
2021-03-23 18:52:02 -0400

    Regular fibonacci O(2^n)

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,11 +2,23 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return this.fibonacci(n-1) + this.fibonacci(n-2);
+        }
     }
 
     kaleb(n){
-        return -1;
+        if (n == 0) {
+            return 2;
+        } else if (n == 1) {
+            return -1;
+        } else {
+            return ((2 * this.kaleb(n-2)) - this.kaleb(n-1))
+        }
     }
 
 }
