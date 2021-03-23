56a45a8f79f87592bf08edd64a1c25889f5a7b9c
ezotic <ezotic@centos8.ezotic.net>
2021-03-23 19:15:46 -0400

    fibonacci is working

--- NachoFibre.js
+++ NachoFibre.js
@@ -3,13 +3,13 @@ class NachoFibre{
 
     fibonacci(n){
       if (n < 3) return 1;
-      else return fibonacci(n - 1) + fibonacci(n - 2);
+      else return this.fibonacci(n - 1) + this.fibonacci(n - 2);
     }
   
 
     kaleb(n){
         if (n < 3) return 1;
-        else return kaleb(n -1 ) + kaleb(n - 2);
+        else return this.kaleb(n -1 ) + this.kaleb(n - 2);
     }
    
 }
