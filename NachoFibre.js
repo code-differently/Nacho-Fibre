fd31fe94ea73a87381ebe0d75499699d72da835c
hina latif <hinalatif25@gmail.com>
2021-03-23 11:06:13 -0400

    first test passed

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,13 +2,28 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+        //return -1;
+
+if (n <=1){
+    return n;
+}else{
+    return this.fibonacci(n-1)+this.fibonacci(n-2);
+}
+
     }
 
     kaleb(n){
-        return -1;
-    }
+        //return -1;
 
-}
+        if (n <=2){
+             return n;
+        
+        }else{
+           return (2* this.kaleb(n-2)) - this.kaleb(n-1);
+
+         }
+
+        }
+    }
 
 module.exports = NachoFibre;
