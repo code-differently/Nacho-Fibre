cd1cc87278e3b667505bd54752a7b2b290e66c8b
spmoore25 <74632580+spmoore25@users.noreply.github.com>
2021-03-23 19:17:30 -0400

    Update

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,8 +2,18 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
-    }
+        if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+        
 
     kaleb(n){
         return -1;
