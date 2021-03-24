7c9ebe3944d016b3e01b6a0a35e0e296d0334307
Shelley L. McIntyre <shelleylmcintyre@Shelleys-Air.home>
2021-03-24 13:00:51 -0400

    added solutions for fibonacci & kaleb problems with 3 out of 6 passing

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,12 +1,30 @@
 
 class NachoFibre{
-
+// if n is at position 1,2 it will equal 1  return 1
+// otherwise retun the sum of the index at position-1 + index at position -2
     fibonacci(n){
-        return -1;
+        if(n<3)
+        return 1;
+        else return this.fibonacci(n-1)+ this.fibonacci(n-2);
     }
 
+    // fibonacci (n, cache){
+    //     let cache = cache|| [];
+    //     if (cache[n])
+    //     return cache[n];
+    //     else {
+    //         if (n < 3) return 1;
+    //         else { cache[n]= fibonacci(n-1, cache)+fibonacci(n-2, cache);
+    //         }
+    //     }
+    //     return cache[n]
+    // }
+
+
     kaleb(n){
-        return -1;
+        if(n=0) return 2;
+        else if (n=1) return -1;
+       else return 2*this.kaleb(n-2)-this.kaleb(n-1);
     }
 
 }
