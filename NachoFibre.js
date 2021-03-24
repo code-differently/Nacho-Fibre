168d474dd623cae9af2cf29e37f7d0679f5005a6
spmoore25 <74632580+spmoore25@users.noreply.github.com>
2021-03-23 20:01:06 -0400

    Update 2

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,24 +1,59 @@
 
 class NachoFibre{
 
-    fibonacci(n){
-        if (n===1) 
-  {
-    return [0, 1];
-  } 
-  else 
-  {
-    var s = fibonacci(n - 1);
-    s.push(s[s.length - 1] + s[s.length - 2]);
-    return s;
-  }
-};
-        
-
-    kaleb(n){
-        return -1;
+fibonacci(n){
+    // fibonacci(0) = 0
+    if (n == 0) {
+     return 0;
+    }
+   
+    // fibonacci(1) = 1
+    if (n == 1) {
+    return 1;
     }
 
-}
+    // fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)
+    let fibN2 = 0
+    let FibN1 = 1
+    let FibN;
+
+    for(let i =2; i <= n; i++){
+        fibN = fibN1 + fibN2;
+        fibN2 = fibN1;
+        fibN1 = fibN;
+    }
+
+    return fibN; 
+
+    }
+  
+
+kaleb(n){
+
+    //kaleb(0) = 2
+    if(n == 0){
+        return 2;
+    }
+
+    //kaleb(1) = -1
+    if(n == 1){
+        return -1;
+    }
+    //kaleb(n) = 2\*kaleb(n-2) - kaleb(n-1)
+    let kalebN2 =2;
+    let kalebN1 =-1;
+    let KalebN;
+
+    for(let i = 2; i <= n; i++){
+        kalebN = 2*kalebN2 - kalebN1;
+        kalebN2 = kalebN1;
+        kalebN1 = kalebN;
+    }
+
+    return kalebN;
+
+    }
+
+};
 
 module.exports = NachoFibre;
