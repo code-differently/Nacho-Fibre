d4825e02ebef26801d0db7f203f5cb51d6f7dd73
Juan Avalos <juanavalos@Juans-MacBook-Air.local>
2021-03-23 21:01:30 -0400

    first push

--- NachoFibre.js
+++ NachoFibre.js
@@ -2,12 +2,36 @@
 class NachoFibre{
 
     fibonacci(n){
-        return -1;
+        //write a formula to replicate fibonacci formula
+        //use the formula that requires golden ration and define golden ratio as a variable
+        var goldenRatio = ( 1 + Math.sqrt(5) ) / 2;
+        //find the the first value needed by formula 
+        var firstValue = Math.pow(goldenRatio, n);
+        //find the the first value needed by formula 
+        var secondValue = Math.pow( (1 - goldenRatio), n);
+        //subtract first value from second value then divide by the square root of 5
+        var value = ( firstValue - secondValue )/Math.sqrt(5);
+        return Math.floor(value);
     }
 
     kaleb(n){
-        return -1;
-    }
+            // var a = 2, b = -1, kterm;
+          
+            // while (n >= 0){
+            //   kterm = a;
+            //   a = b + a;
+            //   b = kterm;
+            //   n--;
+            // }
+          
+            // return b;
+            if (n = 0) { return 2;
+                else { (n = 1) return -1; }
+
+
+                else { 2(kterm(n - 2)) + kerm(n - 1); }
+            }
+          }
 
 }
 
