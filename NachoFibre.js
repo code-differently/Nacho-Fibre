5cbdca03b60c22ad4cb49a62362c4d0fa5a80d05
Michael McMasters <jmcmasters411@gmail.com>
2021-03-24 12:19:37 -0400

    Pass all tests

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,14 +1,34 @@
 
-class NachoFibre{
+class NachoFibre {
 
-    fibonacci(n){
-        return -1;
+    fibonacci(n) {
+        if (n <= 1 || isNaN(n)) return 0;
+
+        // Start at index 1 in fibonacci sequence.
+        let sum = 0, prevSum = 1, prevPrevSum = 0;
+        while (n > 1) {
+            sum = prevSum + prevPrevSum;
+            prevPrevSum = prevSum;
+            prevSum = sum;
+
+            n--;
+        }
+        return sum;
     }
 
-    kaleb(n){
-        return -1;
-    }
+    kaleb(n) {
+        if (n <= 0 || isNaN(n)) return 0;
+        else if (n === 1) return -1;
 
+        // Start at index 2 in Kaleb sequence.
+        let sum = 0, prevSum = -1, prevPrevSum = 2;
+        for (let i = 2; i <= n; i++) {
+            sum = prevPrevSum * 2 - prevSum;
+            prevPrevSum = prevSum;
+            prevSum = sum;
+        }
+        return sum;
+    }
 }
 
 module.exports = NachoFibre;
