1a1a5fdd2482064d52116ca796c5d8a9b7b7295c
IrmIris33 <irmirisortizortiz@outlook.com>
2021-03-24 13:30:44 -0400

    Nacho-Fibre

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,14 +1,14 @@
 
 class NachoFibre{
-
     fibonacci(n){
-        return -1;
+        if(n < 3) return 1;
+        else return fibonacci(n -1) + fibonacci(n-2)
     }
-
     kaleb(n){
-        return -1;
+        if (n < 2) return -1;
+        else return 2*kaleb(n-2) - kaleb(n-1)
     }
-
 }
+nachoFibre.fibonacci(n)
 
 module.exports = NachoFibre;
