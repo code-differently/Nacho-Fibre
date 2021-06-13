1206c8f883d7cf05be435feb979726fa53af0b60
Earl M. Smith <unixsa@gmail.com>
2021-06-13 15:08:36 -0400

    update

--- NachoFibre.js
+++ NachoFibre.js
@@ -8,8 +8,14 @@ class NachoFibre{
   
 
     kaleb(n){
-        if (n < 3) return 1;
-        else return this.kaleb(n -1 ) + this.kaleb(n - 2);
+
+        // kaleb(0) = 2
+        // kaleb(1) = -1
+        // kaleb(n) = 2*kaleb(n-2) - kaleb(n-1)
+
+        if (n = 2) return 1;
+        // else return this.kaleb*2(n -2 ) - this.kaleb(n - 1);
+        else return ((2 * this.kaleb(n-2)) - this.kaleb(n-1));
     }
    
 }
